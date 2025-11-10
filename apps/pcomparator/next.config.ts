18f0fd49b462041aade9f34891f5ca2870278a78
Clément Muth <clementmuth@gmail.com>
2025-11-10 11:08:21 +0100

    feat: implement price suggestion

--- apps/pcomparator/next.config.ts
+++ apps/pcomparator/next.config.ts
@@ -1,23 +1,20 @@
 import type { NextConfig } from "next";
 import withPWAInit from "@ducanh2912/next-pwa";
 
-const readEnvironmentVariable = (key: string, defaultValue: string | undefined = undefined): string => {
+const readEnvironmentVariable = (key: string, defaultValue?: string): string => {
   const value = process.env[key];
-
   if (value === undefined && defaultValue === undefined) {
-    throw new Error(`The environment variable "${key} is missing".`);
+    throw new Error(`The environment variable "${key}" is missing.`);
   }
-
-  return value!;
+  return value ?? defaultValue!;
 };
 
 const PCOMPARATOR_ENV = readEnvironmentVariable("PCOMPARATOR_ENV", "production");
 
 const nextConfig = (): NextConfig => {
-
   if (!["development", "test", "staging", "production"].includes(PCOMPARATOR_ENV)) {
     throw new Error(
-      `The environment variable "PCOMPARATOR_ENV" should have one the value: "development", "test", "staging", "production". Current value: "${PCOMPARATOR_ENV}"`
+      `The environment variable "PCOMPARATOR_ENV" should have one of the following values: "development", "test", "staging", "production". Current value: "${PCOMPARATOR_ENV}"`
     );
   }
 
@@ -27,34 +24,56 @@ const nextConfig = (): NextConfig => {
       PCOMPARATOR_API_ENDPOINT: readEnvironmentVariable("PCOMPARATOR_API_ENDPOINT"),
       OPEN_FOOD_FACT_API_ENDPOINT: readEnvironmentVariable("OPEN_FOOD_FACT_API_ENDPOINT"),
       OPEN_FOOD_FACT_PRICES_API_ENDPOINT: readEnvironmentVariable("OPEN_FOOD_FACT_PRICES_API_ENDPOINT"),
-      ALGOLIA_APP_ID: readEnvironmentVariable("ALGOLIA_APP_ID")!,
-      ALGOLIA_API_KEY: readEnvironmentVariable("ALGOLIA_API_KEY")!,
+      ALGOLIA_APP_ID: readEnvironmentVariable("ALGOLIA_APP_ID"),
+      ALGOLIA_API_KEY: readEnvironmentVariable("ALGOLIA_API_KEY"),
     },
     trailingSlash: false,
     reactStrictMode: true,
     compress: true,
     images: {
-      domains: ["lh3.googleusercontent.com", "firebasestorage.googleapis.com", "vercel.com", "images.openfoodfacts.org", 'gqvzqowza5fybgb2.public.blob.vercel-storage.com']
+      remotePatterns: [
+        {
+          protocol: "https",
+          hostname: "lh3.googleusercontent.com",
+        },
+        {
+          protocol: "https",
+          hostname: "firebasestorage.googleapis.com",
+        },
+        {
+          protocol: "https",
+          hostname: "vercel.com",
+        },
+        {
+          protocol: "https",
+          hostname: "images.openfoodfacts.org",
+        },
+        {
+          protocol: "https",
+          hostname: "gqvzqowza5fybgb2.public.blob.vercel-storage.com",
+        },
+      ],
     },
     experimental: {
-      turbo: {
-        rules: {
-          "*.po": {
-            loaders: ["@lingui/loader"]
-          }
-        }
+      serverActions: {
+        bodySizeLimit: "3mb",
       },
       swcPlugins: [["@lingui/swc-plugin", {}]],
-      serverActions: {
-        bodySizeLimit: "3mb"
-      }
+    },
+    turbopack: {
+      rules: {
+        "*.po": {
+          loaders: ["@lingui/loader"],
+          as: "*.js",
+        },
+      },
     },
   };
 };
 
 const withPWA = withPWAInit({
   dest: "public",
-  disable: false
+  disable: false,
 });
 
 module.exports = withPWA(nextConfig());
